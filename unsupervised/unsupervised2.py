868dcaee303a66a18606117ca2b5f219d0c7244d
jesusr1297 <52177965+Jesusr1297@users.noreply.github.com>
2020-11-13 20:16:04 -0700

    New plots, notebooks and modified files

--- unsupervised/unsupervised2.py
+++ unsupervised/unsupervised2.py
@@ -1,36 +1,31 @@
-# ## Imports
-# Arrays and data handling
-import numpy as np
+from datetime import datetime
+
 import pandas as pd
-
-# Plot and data visualization
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-# regex library
 import re
-
-# Preprocessing
 from gensim.parsing.preprocessing import preprocess_string, strip_tags, strip_punctuation, strip_multiple_whitespaces, \
                                             strip_numeric, remove_stopwords, strip_short
+
+import numpy as np
 from gensim.models import Word2Vec
 
-# Kmeans clustering
 from sklearn import cluster
-# Metrics for evaluation
-# from sklearn import metrics
-# Principal Component Analysis
+
 from sklearn.decomposition import PCA
-# T-distributed Stochastic Neighbor Embedding
+
 from sklearn.manifold import TSNE
 
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-# ## Data Analysis & Cleanup
+start = datetime.now()
+
 fake = pd.read_csv('../data/Fake.csv')
 true = pd.read_csv('../data/True.csv')
 
-print(fake.head(10))
-print(true.head(10))
+print('Fake news preview')
+print(fake.head(15))
+print('\n\n\n True news preview')
+print(true.head(15))
 
 """
 The first issue as seen above is that the True data contains:
@@ -57,7 +52,6 @@ for data in true.text:
 
 true["text"] = cleansed_data
 print(true.head(10))
-print(true.text[7])
 
 """
 Some of the text still contains various characters/words such as:
@@ -84,7 +78,7 @@ true['Label'] = 1
 final_data = pd.concat([fake, true])
 
 # Randomize the rows so its all mixed up
-final_data = final_data.sample(frac=1).reset_index(drop=True)
+final_data = final_data.sample(frac=1, random_state=42).reset_index(drop=True)
 
 # Drop columns not needed
 final_data = final_data.drop(['title', 'text', 'subject', 'date'], axis=1)
@@ -102,115 +96,94 @@ CUSTOM_FILTERS = [lambda x: x.lower(), strip_tags, remove_url, strip_punctuation
                   strip_numeric, remove_stopwords, strip_short]
 
 # Here we store the processed sentences and their label
-processed_data = []
-processed_labels = []
+words_broken_up = [preprocess_string(sentence, CUSTOM_FILTERS) for sentence in final_data.Sentences]
+processed_data = [word for word in words_broken_up if len(word) > 0]
+processed_labels = [label for num, label in enumerate(final_data.Label) if len(words_broken_up[num]) > 0]
 
-for index, row in final_data.iterrows():
-    words_broken_up = preprocess_string(row['Sentences'], CUSTOM_FILTERS)
-    # This eliminates any fields that may be blank after preprocessing
-    if len(words_broken_up) > 0:
-        processed_data.append(words_broken_up)
-        processed_labels.append(row['Label'])
+print(processed_labels[:20])
+print(processed_data[:20])
 
-# ## Word2Vec
+
 # Word2Vec model trained on processed data
-model = Word2Vec(processed_data, min_count=1)
-model.wv.most_similar("country")
-
+model = Word2Vec(sentences=processed_data, min_count=1)
 
 # ## Sentence Vectors
 # Getting the vector of a sentence based on average of all the word vectors in the sentence
 # We get the average as this accounts for different sentence lengths
 
 
-def return_vector(x):
+def return_vector(model_made=None, x=None):
     try:
-        return model[x]
-    finally:
+        return model_made[x]
+    except:
         return np.zeros(100)
 
 
-def sentence_vector(sentence):
-    word_vectors = list(map(lambda x: return_vector(x), sentence))
+def sentence_vector(model_made, sentence):
+    word_vectors = list(map(lambda x: return_vector(model_made, x), sentence))
     return np.average(word_vectors, axis=0).tolist()
 
 
-X = []
-for data_x in processed_data:
-    X.append(sentence_vector(data_x))
+X = [sentence_vector(model, data) for data in processed_data]
 
-
-X_np = np.array(X)
-print(X_np.shape)
-
-
-# ## Clustering
 # Training for 2 clusters (Fake and Real)
 kmeans = cluster.KMeans(n_clusters=2, verbose=1)
 
 # Fit predict will return labels
-clustered = kmeans.fit_predict(X_np)
+clustered = kmeans.fit_predict(X)
 
 testing_df = {'Sentence': processed_data, 'Labels': processed_labels, 'Prediction': clustered}
 testing_df = pd.DataFrame(data=testing_df)
 
-print(testing_df.head(10))
-
-# The results above show that its correctly clustered them in some cases where 0 is fake news and 1 is real news
-
-correct = 0
-incorrect = 0
-for index, row in testing_df.iterrows():
-    if row['Labels'] == row['Prediction']:
-        correct += 1
-    else:
-        incorrect += 1
-
-print("Correctly clustered news: " + str((correct * 100) / (correct + incorrect)) + "%")
+# print(testing_df.head(10))
+testing_df['is_correct'] = np.logical_not(np.logical_xor(testing_df['Labels'], testing_df['Prediction']))
+print(np.sum(testing_df.is_correct)/np.sum(len(testing_df.is_correct))*100)
 
 
-# ## Visualization
-
-# Set as default style
-sns.set()
-
-# PCA of sentence vectors
 pca = PCA(n_components=2)
-pca_result = pca.fit_transform(X_np)
+pca_result = pca.fit_transform(X)
 
 PCA_df = pd.DataFrame(pca_result)
+PCA_df.columns = ['x1', 'x2']
 PCA_df['cluster'] = clustered
-PCA_df.columns = ['x1', 'x2', 'cluster']
+print(PCA_df.head(20))
 
-# T-SNE
+
 tsne = TSNE(n_components=2)
 tsne_result = tsne.fit_transform(pca_result)
 
 TSNE_df = pd.DataFrame(tsne_result)
+TSNE_df.columns = ['x1', 'x2']
 TSNE_df['cluster'] = clustered
-TSNE_df.columns = ['x1', 'x2', 'cluster']
+print(TSNE_df.head(20))
+
+
+color_list = [['darkred', 'indigo'], sns.color_palette('rocket', 2), sns.color_palette('mako', 2)]
 
 # Plots
-fig, ax = plt.subplots(1, 2, figsize=(12, 6))
-sns.scatterplot(data=PCA_df, x='x1', y='x2', hue='cluster', legend="full", alpha=0.5, ax=ax[1])
-sns.scatterplot(data=TSNE_df, x='x1', y='x2', hue='cluster', legend="full", alpha=0.5, ax=ax[0])
-ax[0].set_title('Visualized on TSNE')
-ax[1].set_title('Visualized on PCA')
-plt.show()
+for palette in color_list:
+
+    fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+    sns.set()
+    sns.scatterplot(data=PCA_df, x='x1', y='x2', hue='cluster', legend="full", alpha=0.5, ax=ax[1], palette=palette)
+    sns.scatterplot(data=TSNE_df, x='x1', y='x2', hue='cluster', legend="full", alpha=0.5, ax=ax[0], palette=palette)
+    ax[1].set_title('Visualized on PCA')
+    ax[0].set_title('Visualized on TSNE')
+    plt.show()
 
 
-# ## Custom News Tests
 # Testing with fake news generated from https://www.thefakenewsgenerator.com/
-onion_data = "Flint Residents Learn To Harness Superpowers, But Trump Gets Away Again They developed superpowers after years of drinking from a lead-poisoned water supply. But just having incredible abilities doesn't make them superheroes. Not yet. Donald Trump faced off against the super powered civilians but he got away before they could catch him"
+onion_data = "Flint Residents Learn To Harness Superpowers, But Trump Gets Away Again They developed superpowers after years of drinking from a lead-poisoned water supply. But just having incredible abilities doesn't make them superheroes. Not yet. Donald Trump faced off against the superpowered civilians but he got away before they could catch him"
 
 # Preprocess article
 onion_data = preprocess_string(onion_data, CUSTOM_FILTERS)
 
 # Get sentence vector
-onion_data = sentence_vector(onion_data)
+onion_data = sentence_vector(model, onion_data)
 
 # Get prediction
 print(kmeans.predict(np.array([onion_data])))
+
 
 # News from BBC
 bbc_data = "Nasa Mars 2020 Mission's MiMi Aung on women in space Next year, Nasa will send a mission to Mars. The woman in charge of making the helicopter that will be sent there – which is set to become the first aircraft to fly on another planet – is MiMi Aung. At 16, MiMi travelled alone from Myanmar to the US for access to education. She is now one of the lead engineers at Nasa. We find out what it's like being a woman in space exploration, and why her mum is her biggest inspiration."
@@ -219,7 +192,10 @@ bbc_data = "Nasa Mars 2020 Mission's MiMi Aung on women in space Next year, Nasa
 bbc_data = preprocess_string(bbc_data, CUSTOM_FILTERS)
 
 # Get sentence vector
-bbc_data = sentence_vector(bbc_data)
+bbc_data = sentence_vector(model, bbc_data)
 
 # Get prediction
 print(kmeans.predict(np.array([bbc_data])))
+
+finish = datetime.now()
+print(finish - start)
